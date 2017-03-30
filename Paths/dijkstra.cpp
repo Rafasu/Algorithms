a6fd1b18dc7e65a290cc4772593bf1e85a9f102c
rafasu <rafasu8@gmail.com>
2017-03-29 23:42:12 -0600

    Dijkstra

---
+++ Paths/dijkstra.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std ;
+
+
+
+
+struct arco{
+	int iSrc, iDst  ;
+} ;
+
+
+
+
+void dijkstra(int n){
+	int iInicio, iMin, iVmin = 0 ; 
+	cout << "Nodo Inicio : " << endl ;
+	cin >> iInicio ;
+
+	for(int i = 0; i < n ; i++){
+		
+
+	
+	}
+
+}
+
+
+
+
+int main(){
+
+	return 0 ;
+}
